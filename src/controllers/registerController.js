ce72057aab2ae08013fbf8dde3a23640e04c2705
Anvita10 <123477894+Anvita10@users.noreply.github.com>
2026-04-01 16:54:42 +0530

    Added jwt authentication

---
+++ src/controllers/registerController.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, phoneNo } = await req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phoneNo,
+    });
+    return res.json({ message: "user registered", user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User do  not exist" });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      expiresIn: "2h",
+    });
+
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -10,11 +10,12 @@ const {
 } = require("../controllers/taskController");
 
 const { getAiSuggestion } = require("../controllers/aiSuggestController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", getTask);
-router.post("/", createTask);
-router.delete("/:id", deleteTask);
-router.put("/:id", updateStatus);
-router.get("/categorylist", getCategoryList);
-router.post("/ai-suggest", getAiSuggestion);
+router.get("/", authMiddleware, getTask);
+router.post("/", authMiddleware, createTask);
+router.delete("/:id", authMiddleware, deleteTask);
+router.put("/:id", authMiddleware, updateStatus);
+router.get("/categorylist", authMiddleware, getCategoryList);
+router.post("/ai-suggest", authMiddleware, getAiSuggestion);
 module.exports = router;
