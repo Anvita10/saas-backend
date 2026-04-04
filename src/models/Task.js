f36a6348505424efc5a183fdaa4055d8dd004901
Anvita10 <123477894+Anvita10@users.noreply.github.com>
2026-04-04 15:30:44 +0530

    Update Task.js

--- src/models/Task.js
+++ src/models/Task.js
@@ -6,9 +6,34 @@ const taskSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
     status: {
       type: String,
       default: "ToDo",
+      enum: ["Completed", "Pending", "In-Progress", "ToDo", "Rejected"],
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    assignedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    workspace: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
     },
     category: {
       type: String,
