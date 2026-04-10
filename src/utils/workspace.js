4aeec31960ebbe1f67b595300a3a8ae50bcd6a98
Anvita10 <123477894+Anvita10@users.noreply.github.com>
2026-04-10 20:42:09 +0530

    making the utility flexible

--- src/utils/workspace.js
+++ src/utils/workspace.js
@@ -1,18 +1,44 @@
-const Workspace=require("../models/Workspace")
+const Workspace = require("../models/Workspace");
 
-exports.getWorkspaceAndCheckMember=async(workspaceId,userId)=>{
-  const workspace=await Workspace.findById(workspaceId)
+exports.getWorkspaceAndCheckMember = async (
+  workspaceId,
+  userId,
+  options = {},
+) => {
+  const {
+    populateMembers = false,
+    populateOwner = false,
+    selectFields = null,
+  } = options;
+
+  let query = Workspace.findById(workspaceId);
+
+  if (selectFields) {
+    query = query.select(selectFields);
+  }
+
+  if (populateMembers) {
+    query = query.populate("members.userId", "name email");
+  }
+
+  if (populateOwner) {
+    query = query.populate("owner", "name");
+  }
+
+  const workspace = await query;
+
   if (!workspace) {
     return { error: { status: 404, message: "Workspace not found" } };
   }
 
-  const isMember = workspace.members.some(
-    val => val.userId?.toString() === userId
-  );
+  const isMember = workspace.members.some((val) => {
+    const memberId = val.userId?._id || val.userId;
+    return memberId.toString() === userId;
+  });
 
   if (!isMember) {
     return { error: { status: 403, message: "Not a workspace member" } };
   }
 
-  return {workspace}
-}
+  return { workspace };
+};
