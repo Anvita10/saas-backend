5d74172469f3690ad21235d1b9ad9b167439e352
Anvita10 <123477894+Anvita10@users.noreply.github.com>
2026-04-12 20:26:10 +0530

    Update taskController.js

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,53 +1,44 @@
 const Task = require("../models/Task");
-const {getWorkspaceAndCheckMember}=require("../utils/workspace")
-const {success,error}=require("../utils/response")
-
-const category = [
-  "Tech",
-  "Fitness",
-  "Learning",
-  "Productivity",
-  "Personal",
-  "Fun",
-  "Career",
-];
+const { getWorkspaceAndCheckMember } = require("../utils/workspace");
+const { success, error } = require("../utils/response");
 
 exports.getTask = async (req, res, next) => {
   try {
+    const { workspaceId } = req.params;
+    const userId = req.user.id;
 
-    const {workspaceId}=req.params
-    const userId=req.user.id
+    const { workspace, error: wsError } = await getWorkspaceAndCheckMember(
+      workspaceId,
+      userId,
+    );
 
-    const {workspace,error:wsError}= await getWorkspaceAndCheckMember(workspaceId,userId)
-
-     if (wsError) {
-      return error(res,wsError.message,wsError.status)
+    if (wsError) {
+      return error(res, wsError.message, wsError.status);
     }
 
-    //getting the filters  
-    const {status,assignedTo,dueDate,priority}=req.query
-    
+    //getting the filters
+    const { status, assignedTo, dueDate, priority } = req.query;
 
     //building the filter
     let parsedDate;
-    if(dueDate)
-    {
-      parsedDate=new Date(dueDate)
+    if (dueDate) {
+      parsedDate = new Date(dueDate);
       if (isNaN(parsedDate.getTime())) {
         return error(res, "Invalid date format", 400);
       }
     }
-    const filter={
-      workspace:workspaceId,
-      ...(status && {status}),
-      ...(priority && {priority}),
-      ...(assignedTo && {assignedTo}),
-      ...(parsedDate && {dueDate:{$lte :parsedDate}})
-    }
- 
-    const tasks = await Task.find(filter);
-    return success(res,tasks)
+    const filter = {
+      workspace: workspaceId,
+      ...(status && { status }),
+      ...(priority && { priority }),
+      ...(assignedTo && { assignedTo }),
+      ...(parsedDate && { dueDate: { $lte: parsedDate } }),
+    };
 
+    const tasks = await Task.find(filter)
+      .populate("assignedTo", "name")
+      .populate("assignedBy", "name");
+    return success(res, tasks);
   } catch (err) {
     next(err);
   }
@@ -55,32 +46,45 @@ exports.getTask = async (req, res, next) => {
 
 exports.createTask = async (req, res, next) => {
   try {
-    const {workspaceId}=req.params
-    const userId=req.user.id
+    const { workspaceId } = req.params;
+    const userId = req.user.id;
 
-    const {workspace,error:wsError}=await getWorkspaceAndCheckMember(workspaceId,userId)
+    const { workspace, error: wsError } = await getWorkspaceAndCheckMember(
+      workspaceId,
+      userId,
+    );
 
-     if (wsError) {
-      return error(res,wsError.message,wsError.status)
+    if (wsError) {
+      return error(res, wsError.message, wsError.status);
     }
 
-    const {title,description,assignedTo,dueDate,priority}  =req.body
+    const { title, description, assignedTo, dueDate, priority } = req.body;
 
     //validate the inputs
     if (!title) {
-      return error(res,"Title is required",400) 
+      return error(res, "Title is required", 400);
     }
 
     // check is assiggned to a member of workspace
-    if(assignedTo)
-    {
-      const isAssignedTo=workspace.members.some((val)=>val.userId.toString()===assignedTo.toString())
-      if(!isAssignedTo) return error(res,"Assigned user is not part of workspace",403)
+    if (assignedTo) {
+      const isAssignedTo = workspace.members.some(
+        (val) => val.userId.toString() === assignedTo.toString(),
+      );
+      if (!isAssignedTo)
+        return error(res, "Assigned user is not part of workspace", 403);
     }
 
-    const task = new Task({title,description,assignedTo,dueDate,priority,assignedBy:userId,workspace:workspaceId});
+    const task = new Task({
+      title,
+      description,
+      assignedTo,
+      dueDate,
+      priority,
+      assignedBy: userId,
+      workspace: workspaceId,
+    });
     await task.save();
-    return success(res,task,201)
+    return success(res, task, 201);
   } catch (err) {
     next(err);
   }
@@ -88,68 +92,85 @@ exports.createTask = async (req, res, next) => {
 
 exports.deleteTask = async (req, res, next) => {
   try {
-    const {workspaceId,taskId}=req.params
-    const userID=req.user.id
+    const { workspaceId, taskId } = req.params;
+    const userID = req.user.id;
 
-   const {workspace,error:wsError}=await getWorkspaceAndCheckMember(workspaceId,userID)
+    const { workspace, error: wsError } = await getWorkspaceAndCheckMember(
+      workspaceId,
+      userID,
+    );
 
-     if (wsError) {
-      return error(res,wsError.message,wsError.status)
+    if (wsError) {
+      return error(res, wsError.message, wsError.status);
     }
 
-      //validate if the task exist in workspace 
-    const task=await Task.findOne({_id:taskId,workspace:workspaceId})  
-    if(!task) return error(res,"Task Not exist",404)
+    //validate if the task exist in workspace
+    const task = await Task.findOne({ _id: taskId, workspace: workspaceId });
+    if (!task) return error(res, "Task Not exist", 404);
 
     //Validate user access
-    const hasPermission=workspace.owner.toString()===userID || workspace.members.some(val=>val.userId.toString()===userID && val.role==="admin")  
-    if(!hasPermission) return error(res,"user do not have access",403)
+    const hasPermission =
+      workspace.owner.toString() === userID ||
+      workspace.members.some(
+        (val) => val.userId.toString() === userID && val.role === "admin",
+      );
+    if (!hasPermission) return error(res, "user do not have access", 403);
 
-    await Task.findByIdAndDelete(taskId)  
-    return res.status(204).send()
+    await Task.findByIdAndDelete(taskId);
+    return res.status(204).send();
   } catch (err) {
     next(err);
   }
 };
 
 exports.updateTask = async (req, res, next) => {
-  const { status,assignedTo } = req.body;
-  const {workspaceId,taskId}=req.params
-  const userID=req.user.id
+  const { status, assignedTo } = req.body;
+  const { workspaceId, taskId } = req.params;
+  const userID = req.user.id;
 
+  try {
+    const { workspace, error: wsError } = await getWorkspaceAndCheckMember(
+      workspaceId,
+      userID,
+    );
 
-  try{
-  const {workspace,error:wsError}=await getWorkspaceAndCheckMember(workspaceId,userID)
+    if (wsError) {
+      return error(res, wsError.message, wsError.status);
+    }
 
-  if (wsError) {
-    return error(res,wsError.message,wsError.status)
-  }
+    //validate if the task exist in workspace
+    const task = await Task.findOne({ _id: taskId, workspace: workspaceId });
+    if (!task) return error(res, "Task Not exist", 404);
 
-    //validate if the task exist in workspace 
-  const task=await Task.findOne({_id:taskId,workspace:workspaceId})  
-  if(!task) return error(res,"Task Not exist",404)
+    //validate user permission to update the task
+    const hasPermission =
+      task.assignedTo?.toString() === userID ||
+      workspace.owner.toString() === userID ||
+      workspace.members.some(
+        (val) => val.userId.toString() === userID && val.role === "admin",
+      );
+    if (!hasPermission)
+      return error(res, "user is not allowed to update the task", 403);
 
-  //validate user permission to update the task 
-  const hasPermission=task.assignedTo?.toString()===userID || workspace.owner.toString()===userID || workspace.members.some(val=>val.userId.toString()===userID && val.role==="admin")
-  if(!hasPermission) return error(res,"user is not allowed to update the task",403)
+    const update = {};
+    if (status) update.status = status;
 
-  const update={}
-  if(status)update.status=status
-
-  //check weather the assigned to member of workspace
-  if(assignedTo!==undefined && assignedTo!==null){
-    const isAssignedToMember=workspace.members.some(val=>val.userId.toString()===assignedTo.toString())
-    if(!isAssignedToMember) return error(res,"Assigned user is not part of workspace",400)
-    update.assignedTo=assignedTo
-    update.assignedBy=userID
-   } 
-    const updatedTask=await Task.findByIdAndUpdate(taskId, update ,{new:true, runValidators:true});
-    return success(res,updatedTask)
+    //check weather the assigned to member of workspace
+    if (assignedTo !== undefined && assignedTo !== null) {
+      const isAssignedToMember = workspace.members.some(
+        (val) => val.userId.toString() === assignedTo.toString(),
+      );
+      if (!isAssignedToMember)
+        return error(res, "Assigned user is not part of workspace", 400);
+      update.assignedTo = assignedTo;
+      update.assignedBy = userID;
+    }
+    const updatedTask = await Task.findByIdAndUpdate(taskId, update, {
+      new: true,
+      runValidators: true,
+    });
+    return success(res, updatedTask);
   } catch (err) {
     next(err);
   }
-};
-
-exports.getCategoryList = (req, res) => {
-  res.json({ data: category });
 };
