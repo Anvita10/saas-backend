704d7e8b7d3bbd35912ceb20f210622748cae08b
Anvita10 <123477894+Anvita10@users.noreply.github.com>
2026-04-14 17:07:21 +0530

    success message added

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,13 +1,19 @@
-exports.success=(res,data,status=200)=>{
-    return res.status(status).json({
-        success:true,
-        data
-    })
-}
+exports.success = (
+  res,
+  data,
+  status = 200,
+  message = "Data added successfully",
+) => {
+  return res.status(status).json({
+    success: true,
+    data,
+    message,
+  });
+};
 
-exports.error=(res,message,status=500)=>{
-    return res.status(status).json({
-        success:false,
-        message
-    })
-}
+exports.error = (res, message, status = 500) => {
+  return res.status(status).json({
+    success: false,
+    message,
+  });
+};
